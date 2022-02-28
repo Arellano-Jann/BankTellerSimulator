47b1d13a468aa72b0cf6922ec3b328f799cb69e1
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-02-28 15:24:18 -0800

    Queue Stubs

--- ArrayQueue.cpp
+++ ArrayQueue.cpp
@@ -1,7 +1,29 @@
+template<typename T>
 ArrayQueue::ArrayQueue(/* args */)
 {
 }
 
+template<typename T>
 ArrayQueue::~ArrayQueue()
 {
+}
+
+template<typename T>
+bool PriorityQueue::isEmpty() const{
+
+}
+
+template<typename T>
+bool PriorityQueue::enqueue(const T& newEntry){
+
+}
+
+template<typename T>
+bool PriorityQueue::dequeue(){
+
+}
+
+template<typename T>
+T PriorityQueue::peekFront(){
+
 }
--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -5,3 +5,23 @@ PriorityQueue::PriorityQueue(/* args */)
 PriorityQueue::~PriorityQueue()
 {
 }
+
+template<typename T>
+bool PriorityQueue::isEmpty() const{
+
+}
+
+template<typename T>
+bool PriorityQueue::enqueue(const T& newEntry){
+
+}
+
+template<typename T>
+bool PriorityQueue::dequeue(){
+
+}
+
+template<typename T>
+T PriorityQueue::peekFront(){
+
+}
