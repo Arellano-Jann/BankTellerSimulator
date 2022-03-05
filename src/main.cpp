82687dc5a0888320d1db1e81664aa3708d13da46
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-04 22:50:16 -0800

    Cleaned main. Now for cleaning everything else

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,6 @@
  * 
  */
 
-// Pseudo Code
-
-// Takes in file
-// Puts each line into a priority queue.
-// Has a while loop
-	// The while loop iterates through based on if there is still events on the priority queue
-	// If the teller is available, set the customers arrival time to the current time and pop the prio queue.
-		// When the customer departs, set the current time to += the waiting time and check if front of queue arrival time is less than the current time.
-			// If front of queue arrival time is less than the current time, take the customer to the teller. This is done by setting the customer.arrivalTime to the current time and popping the front of the queue.
-			// Else output “Waiting for customer” and then set customer.arrivalTime to the current time. Take the customer to the teller and pop the front of the queue.
-	// If the teller is not available, put in the back of queue
-// Note that when popping prio queue, print a statement listing type of event and current time/arrival time.
 #include <iostream> 
 #include <string>
 #include <fstream> 
@@ -30,13 +18,13 @@
 #include "../headers/PriorityQueue.h"
 #include "../headers/EventTracker.h"
 
-bool fileParser(PriorityQueue<Customer> &fileQueue); // this should be a prio q
+bool fileParser(PriorityQueue<Customer> &fileQueue);
 bool depart();
 bool arrive();
 void output(PriorityQueue<EventTracker> EventQueue);
 
-PriorityQueue<Customer> fileQueue; // initial queue
-ArrayQueue<Customer> bankQueue; // bank line
+PriorityQueue<Customer> fileQueue;
+ArrayQueue<Customer> bankQueue;
 PriorityQueue<EventTracker> EventQueue;
 bool isTellerAvailable = true;
 static int currentTime = 0;
@@ -44,21 +32,11 @@ static int currentTime = 0;
 
 
 int main(){
-	// std::cout << "What's the file name? ";
-	// std::cin >> filename;
-	// fileParser(fileQueue);
 	if (fileParser(fileQueue)){
 		while (!fileQueue.isEmpty()){
 			arrive();
-			std::cout << "fileQArrive ";
-		}
-		std::cout << "AfterArrivals ";
-		if (bankQueue.isEmpty()){
-			std::cout << "bankQEmpty ";
-			// depart();
 		}
 		while (!bankQueue.isEmpty()){
-			std::cout << "bankQDepart ";
 			depart();
 		}
 	}
@@ -68,13 +46,13 @@ int main(){
 bool fileParser(PriorityQueue<Customer> &fileQueue){
 	std::string filename = "file.txt";
 	std::cout << "What's the file name? ";
-	// std::cin >> filename;
+	std::cin >> filename;
 
 	std::ifstream file(filename);
 	if (file.is_open()){
 		int arrivalTime, waitingTime;
 		while (file >> arrivalTime >> waitingTime){
-			Customer customer(arrivalTime, waitingTime); // if this doesn't work use getters and setters
+			Customer customer(arrivalTime, waitingTime);
 			fileQueue.enqueue(customer);
 		}
 		file.close();
@@ -88,40 +66,29 @@ bool fileParser(PriorityQueue<Customer> &fileQueue){
 bool depart(){
 		if(!bankQueue.isEmpty()){
 			Customer customer = bankQueue.peekFront(); // sets the front of the bank q to customer
-			// currentTime += customer.getWaitingTime(); // calcs departure time
 			if ( currentTime < customer.getArrivalTime() ) currentTime = customer.getArrivalTime();
-			currentTime += customer.getWaitingTime(); // calcs departure time
-			EventTracker departureEvent(currentTime, 0, "departure"); // creates a newEvent for each customers type
-			EventQueue.enqueue(departureEvent);
+			currentTime += customer.getWaitingTime(); // calcs departure time 
+			EventQueue.enqueue(EventTracker(currentTime, 0, "departure")); // creates a newEvent for each customers type
 			bankQueue.dequeue();
-			std::cout << "DepartedFromBankQ ";
 			return true;
 		}
-		std::cout << "DepartFailed ";
 	return false;
 }
-bool arrive(){ // tbh i don't get the variables
+bool arrive(){
 	if (isTellerAvailable){
 		currentTime = fileQueue.peekFront().getArrivalTime(); // sets the current time to arrival time of customer
-		// EventTracker arrivalEvent(currentTime, 0, "arrival");
-		if ( EventQueue.enqueue(EventTracker(currentTime, 0, "arrival")) ) { std::cout << "ArrivalEventQd "; }
+		EventQueue.enqueue(EventTracker(currentTime, 0, "arrival"));
 		currentTime += fileQueue.peekFront().getWaitingTime(); // sets the current time to actual departureTime
-		// EventTracker departureEvent(currentTime, 0, "departure"); // creates a newEvent for each customers type
-		if ( EventQueue.enqueue(EventTracker(currentTime, 0, "departure")) ) { std::cout << "DepartureEventQd "; }
+		EventQueue.enqueue(EventTracker(currentTime, 0, "departure")); // creates a newEvent for each customers type
 		fileQueue.dequeue(); // takes off the customer off the queue
 		isTellerAvailable = false;
-		std::cout << "FinFirstArrive ";
 		return true;
 	}
 	if (!fileQueue.isEmpty()){
-		if( bankQueue.enqueue(fileQueue.peekFront()) ){ std::cout << "BankQEnqueued "; }
-		// bankQueue.enqueue(fileQueue.peekFront()); // assuming that the event has a proper arrival time etc
+		bankQueue.enqueue(fileQueue.peekFront()); // assuming that the event has a proper arrival time etc
 		int arrivalTime = fileQueue.peekFront().getArrivalTime();
-		// EventTracker arrivalEvent(arrivalTime, 0, "arrival");
-		// EventQueue.enqueue(arrivalEvent);
-		if ( EventQueue.enqueue(EventTracker(arrivalTime, 0, "arrival")) ){ std::cout << "ArrivalSubQd "; }
+		EventQueue.enqueue(EventTracker(arrivalTime, 0, "arrival"));
 		fileQueue.dequeue(); // takes off the customer off the queue
-		std::cout << "FinSubsequentArrive ";
 		return true;
 	}
 	return false;
